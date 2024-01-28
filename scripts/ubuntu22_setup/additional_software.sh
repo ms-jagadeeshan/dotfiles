0748816c5ba93c14b771577a4820dd57b506862e
Jagadeeshan S <jagadeeshanmsj@gmail.com>
2024-01-28 14:21:51 +0530

    add add alias and ros setup

--- scripts/ubuntu22_setup/additional_software.sh
+++ scripts/ubuntu22_setup/additional_software.sh
@@ -2,3 +2,4 @@
 
 sudo apt install speedtest-cli
 sudo apt install python3-venv python3-pip
+sudo apt isntall terminator xterm
--- scripts/ubuntu22_setup/ros.sh
+++ scripts/ubuntu22_setup/ros.sh
@@ -16,4 +16,4 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-a
 sudo apt update && sudo apt upgrade
 
 sudo apt install ros-humble-ros-base
-
+sudo apt install ros-humble-gz
