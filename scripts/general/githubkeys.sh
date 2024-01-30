343fc524147455394d88c8fd23ee05d0cf136d08
Jagadeeshan S <jagadeeshanmsj@gmail.com>
2024-01-30 21:36:11 +0530

    add copy keys in github setup

--- scripts/general/githubkeys.sh
+++ scripts/general/githubkeys.sh
@@ -3,4 +3,5 @@ pushd ~/.ssh || return
 ssh-keygen -t ed25519 -C "jagadeeshanmsj@gmail.com" -f ~/.ssh/githubkey
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/githubkey
+xclip -sel c < ~/.ssh/githubkey.pub
 popd || return
